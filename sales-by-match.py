2b01fff073e73203924ed663c9c1199b15037520
Gurubacharya <sajit1@kpmg.com.au>
2023-04-04 21:27:36 +1000

    added sales by match code

---
+++ sales-by-match.py
@@ -0,0 +1,41 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'sockMerchant' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. INTEGER_ARRAY ar
+#
+
+def sockMerchant(n, ar):
+    sock_colorcount = [0]*101
+    total_pairs = 0
+    
+    for sock_number in ar:
+        sock_colorcount[sock_number] += 1
+
+    for color_count in sock_colorcount:
+        total_pairs += int(color_count/2)
+            
+    return total_pairs
+            
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = sockMerchant(n, ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
